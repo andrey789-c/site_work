7256eed80d7e93d8184a20d2e36e68fc809b28ae
andrey789-c <77191683+andrey789-c@users.noreply.github.com>
2022-07-20 15:21:07 +1000

    Add files via upload

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux";
+
+import styles from '../styles/Profile.module.css'
+
 function Profile() {
+    const user = useSelector(state => state.NameReducer)
+    console.log(user);
+
     return (
-        <div className="profile">
-            Это Василий он сантехник
+        <div className={styles.profile}>
+            <div className={styles.name}>{user.name}</div>
+            <div className={styles.age}>{user.email}</div>
         </div>
     );
 }
